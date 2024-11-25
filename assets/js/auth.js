9b5bdfdd5ed3cd51778dc9dbb722b4e596096ac9
HiImNVH <102985887+HiImNVH@users.noreply.github.com>
2024-11-25 15:27:38 +0700

    update jss

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,4 +1,3 @@
-// Chuyển qua lại giữa đăng nhập và đăng ký
 document.getElementById('switch-to-signup').addEventListener('click', function() {
     document.getElementById('login-form').classList.remove('active');
     document.getElementById('signup-form').classList.add('active');
@@ -9,9 +8,13 @@ document.getElementById('switch-to-login').addEventListener('click', function() 
     document.getElementById('login-form').classList.add('active');
 });
 
-// Điều hướng về trang index.html khi nhấn nút đăng nhập hoặc đăng ký
 document.getElementById('login-btn').addEventListener('click', function() {
     alert('Đăng nhập thành công!');
+    window.location.href = 'index-logined.html';
+});
+
+document.getElementById('signup-btn').addEventListener('click', function() {
+    alert('Đăng ký thành công!');
     window.location.href = 'index-logined.html';
 });
 
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function logout() {
+    alert("Bạn đã đăng xuất.");
+    window.location.href = "index.html";
+}
+function addToCart() {
+    alert("Thêm vào giỏ thành công!");
+}
